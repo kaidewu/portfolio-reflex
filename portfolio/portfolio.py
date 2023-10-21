56efe87f38e2959fa3e1df4d95f4c122de65c5f3
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-21 20:43:14 +0200

    Add contact page in the reflex app

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 from portfolio.pages.index import index
 from portfolio.pages.works import works
+from portfolio.pages.contact import contact
 
 from portfolio.state.state import State
 
@@ -12,4 +13,5 @@ app = rx.App(
     )
 app.add_page(index)
 app.add_page(works)
+app.add_page(contact)
 app.compile()
