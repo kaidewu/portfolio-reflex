1d65bfd3872d276f658e2a60bae631beb59a948f
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-22 23:43:12 +0200

    Create index page

--- portfolio/pages/index.py
+++ portfolio/pages/index.py
@@ -2,11 +2,28 @@ import reflex as rx
 
 from portfolio.components.layout.main import main
 
-def index():
+def index() -> rx.Component:
     return main(
         rx.container(
             rx.box(
-
+                rx.heading(
+                    "Hey, I'm Kaide",
+                    as_="h3",
+                    variant="section-title"
+                ),
+                rx.text(
+                    "I'm actually working in ",
+                    rx.link(
+                        "Laberit",
+                        href="https://www.laberit.com/",
+                        passHref=True,
+                        scroll={False},
+                        fontWeight="bold"
+                    ),
+                    " as a Consultant and Implanter of an Hospital Information System (HIS).",
+                    textAlign="justify"
+                ),
+                delay={0.1}
             )
         )
     )
