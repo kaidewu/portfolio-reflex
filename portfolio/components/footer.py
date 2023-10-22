3088071a61db0e21b6a722f7e2b504ace57f38e9
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-22 23:40:34 +0200

    Create footer component

--- portfolio/components/footer.py
+++ portfolio/components/footer.py
@@ -1,4 +1,11 @@
 import reflex as rx
 
-def footer():
-    return rx.box()
+from datetime import date
+
+def footer() -> rx.Component:
+    return rx.box(
+        f"© {date.today().year} Kaide Wu. All rights Reserved.",
+        alignItems="center",
+        opacity={0.4},
+        fontSize="sm"
+    )
