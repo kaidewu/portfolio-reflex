5faf632c568376e69a667028b67eedcf2c22e7ba
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-22 23:42:36 +0200

    Adding project page, contact page, and style.css in project

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 from portfolio.pages.index import index
 from portfolio.pages.works import works
+from portfolio.pages.projects import projects
 from portfolio.pages.contact import contact
 
 from portfolio.state.state import State
@@ -10,8 +11,12 @@ from portfolio.state.state import State
 # Add state and page to the app.
 app = rx.App(
     state=State,
+    stylesheets={
+        "/project_style.css"
+    }
     )
 app.add_page(index)
 app.add_page(works)
+app.add_page(projects)
 app.add_page(contact)
 app.compile()
