db7ff6bc999af0815b252c2db50517430221deb1
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-24 23:19:07 +0200

    Adding new projects pages

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,7 +2,11 @@ import reflex as rx
 
 from portfolio.pages.index import index
 from portfolio.pages.works import works
-from portfolio.pages.projects import projects
+from portfolio.pages.projects.projects import projects
+from portfolio.pages.projects.myhomecloud import my_home_cloud
+from portfolio.pages.projects.portfolio_reflex import portfolio_reflex
+from portfolio.pages.projects.cloudy import cloudy
+from portfolio.pages.projects.sqly import sqly
 from portfolio.pages.contact import contact
 
 from portfolio.state.state import State
@@ -18,5 +22,9 @@ app = rx.App(
 app.add_page(index)
 app.add_page(works)
 app.add_page(projects)
+app.add_page(my_home_cloud)
+app.add_page(portfolio_reflex)
+app.add_page(cloudy)
+app.add_page(sqly)
 app.add_page(contact)
 app.compile()
