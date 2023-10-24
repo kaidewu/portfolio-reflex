9e8770044aff47e5c81f7c7408b9bbe709f97374
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-24 23:15:57 +0200

    Create constants.py to contain constants

---
+++ portfolio/constants.py
@@ -0,0 +1,4 @@
+GITHUB_URL = "https://github.com/kaidewu"
+LINKEDIN_URL = "https://linkedin.com/in/kaidewu"
+MAIL = "kaidewu@outlook.es"
+GITHUB_PORTFOLIO_REFLEX_URL = "https://github.com/kaidewu/portfolio-reflex"
