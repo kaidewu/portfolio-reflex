8a6691838531889e27b3dfe091ec4c1ea6927ede
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-10-29 18:51:32 +0100

    Adding new layout of projects items

--- portfolio/pages/projects/portfolio_reflex.py
+++ portfolio/pages/projects/portfolio_reflex.py
@@ -1,11 +1,17 @@
 import reflex as rx
 
 from portfolio.components.layout.main import main
+from portfolio.components.layout.project_information import project_information
 
 @rx.page(route="/projects/portfolio-reflex")
 def portfolio_reflex() -> rx.Component:
     return main(
-        rx.container(
-            "Portfolio Reflex Page!"
+        project_information(
+            "Portfolio Reflex",
+            rx.box(
+                rx.text(
+                    "Welcome to my portfolio build with Reflex"
+                )
+            )
         )
     )
