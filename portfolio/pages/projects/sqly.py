c2f6c0c6b02d1cbdff189a9273de681f58db77ff
Kaide Wu <71648366+kaidewu@users.noreply.github.com>
2023-11-05 17:31:16 +0100

    Still working

--- portfolio/pages/projects/sqly.py
+++ portfolio/pages/projects/sqly.py
@@ -1,6 +1,7 @@
 import reflex as rx
 
 from portfolio.components.layout.main import main
+from portfolio.components.projects import title
 
 @rx.page(route="/projects/sqly")
 def sqly() -> rx.Component:
