f9b79e6d07fd1c6077c7f416bb8f2f86b9de165c
Kaide <kaidewu@outlook.es>
2023-11-11 11:57:08 +0100

    Adding more information about me and my work (#15)
    * Adding github icon in the menu list
    
    * Working in 'About me' section
    
    * Working in my work life page

--- portfolio/pages/works.py
+++ portfolio/pages/works.py
@@ -7,6 +7,17 @@ from portfolio.components.project_item import project_item
 def works() -> rx.Component:
     return main(
         rx.container(
-            "Works Page"
+            rx.text(
+                "I'm actually working in ",
+                rx.link(
+                    "Laberit",
+                    href="https://www.laberit.com/",
+                    passHref=True,
+                    scroll="false",
+                    fontWeight="bold"
+                ),
+                " as a Consultant and Implanter of an Hospital Information System (HIS).",
+                textAlign="justify"
+            ),
         )
     )
