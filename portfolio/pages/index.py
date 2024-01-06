e853aa903419c21fd4fbed791613e4f9507dbfe1
Kaide Wu <kaidewu@outlook.es>
2024-01-06 18:24:08 +0100

    create projects cards using github API

--- portfolio/pages/index.py
+++ portfolio/pages/index.py
@@ -76,22 +76,22 @@ def index() -> rx.Component:
             rx.container(
                 rx.foreach(
                     Github.user_repos,
-                    lambda repo_data: rx.link_box(
+                    lambda repository: rx.link_box(
                         rx.box(
                             rx.box(
                                 rx.box(
-                                    rx.span(repo_data.name),
+                                    rx.span(repository[4]),
                                     rx.span(class_name="w-px flex-1 bg-blue-900/10"),
-                                    rx.span(),
+                                    rx.span(repository[5]),
                                     class_name="flex items-center justify-between gap-4 text-xs font-bold uppercase"
                                 ),
                                 as_="time",
-                                date_time="",
+                                date_time=f"{repository[3]}",
                                 class_name="rotate-180 p-2 [writing-mode:_vertical-lr]"
                             ),
                             rx.box(
                                 rx.image(
-                                    alt=f"",
+                                    alt=f"{repository[0]}",
                                     src="/example_image.jpg",
                                     max_w='{{ base: "100%", sm: "200px" }}',
                                     class_name="aspect-square h-full w-full object-cover"
@@ -100,21 +100,31 @@ def index() -> rx.Component:
                             rx.box(
                                 rx.box(
                                     rx.link_overlay(
-                                        rx.heading(
-                                            "",
-                                            class_name="font-bold uppercase",
-                                            as_="h4"
+                                        rx.text(
+                                            repository[0],
+                                            class_name="font-bold uppercase font-size-20"
                                         ),
-                                        href="#"
+                                        href=f"{repository[2]}",
+                                        is_external=True
                                     ),
                                     rx.text(
-                                        "",
+                                        repository[1],
                                         class_name="mt-2 line-clamp-3 text-sm/relaxed"
+                                    ),
+                                    rx.box(
+                                        rx.foreach(
+                                            [repository[6]],
+                                            lambda topic: rx.span(
+                                                topic,
+                                                class_name="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+                                            ),
+                                        ),
+                                        class_name="mt-4 flex flex-wrap gap-1"
                                     ),
                                 ),
                                 class_name="flex flex-1 flex-col justify-between p-3"    
                             ),
-                            class_name="flex transition hover:shadow-xl pt-5"
+                            class_name="flex transition hover:shadow-xl pt-5 rounded-lg"
                         ),
                         as_="article"
                     ),
--- portfolio/state/github.py
+++ portfolio/state/github.py
@@ -1,51 +1,71 @@
 from __future__ import annotations
 
 import reflex as rx
-import json
 import httpx
 from datetime import datetime
-from typing import List
-from ..constants import GITHUB_API_TOKEN, GITHUB_API_URL
+from typing import NamedTuple
+from ..constants import GITHUB_API_URL
 
-class RepoData(rx.Base):
+def github_connection():
+    try:
+        response =  httpx.get(
+            GITHUB_API_URL
+        )
+        if response.status_code == 200:
+            user_repositories = response.json()
+            return user_repositories
+        return False
+    except Exception as err:
+        raise Exception(err)
+
+class RepoData(NamedTuple):
     name: str
     description: str
     repo_url: str
     created_at: str
     year: str
     month_day: str
+    topics: list
 
 class Github(rx.State):
     """Github Repositories"""
-    response = json.loads(httpx.get(
-                GITHUB_API_URL,
-                headers={
-                    "Accept": "application/vnd.github+json",
-                    "Authorization": f"Bearer {GITHUB_API_TOKEN}",
-                    "X-GitHub-Api-Version": "2022-11-28", # The date when the API version was released. https://docs.github.com/en/rest/about-the-rest-api/api-versions?apiVersion=2022-11-28
-                }
-            ).text)
-
-    languages: List[str] = ["python", "html", "javascript", "typescript", "css"]
+    
+    languages: list[str] = ["Python", "HTML", "JavaScript", "TypeScript", "CSS"]
+    user_repositories = github_connection()
     
     @rx.var
-    def user_repos(self) -> List[RepoData]:
+    def user_repos(self) -> list[RepoData]:
+        """Get information of user public repositories
+
+        Args:
+            None
+
+        Returns:
+            if it is no error returns List[RepoData]:
+                name: str
+                description: str
+                repo_url: str
+                created_at: str
+                year: str
+                month_day: str
+            else:
+                error message
         """
-        :params = None
-        :description = Returns in JSON format, the all repositories of the user
-        :own annotation = Return general information of the user repositories
-        """
-        repos_data: List[RepoData] = []
-        
-        for i in range(len(self.response)):
-            if (self.response[i]["language"] in self.languages):
-                repo_tuple = RepoData(
-                name=self.response[i]["name"],
-                description=self.response[i]["description"],
-                repo_url=self.response[i]["html_url"],
-                created_at=self.response[i]["created_at"],
-                year=str(datetime.strptime(self.response[i]["created_at"], "%Y-%m-%dT%H:%M:%SZ").year),
-                month_day=str(datetime.strptime(self.response[i]["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%b %d"))
-                )
-            repos_data.append(repo_tuple)
-        return repos_data
+        repos_data: list[RepoData] = []
+        try:
+            for repo in self.user_repositories:
+                if (repo["language"] in self.languages) and (repo["visibility"] == "public"):
+                    repos_data.append(
+                        RepoData(
+                            name=repo["name"] if repo else "", # 0
+                            description=repo["description"] if repo else "", # 1
+                            repo_url=repo["html_url"] if repo else "", # 2
+                            created_at=repo["created_at"] if repo else "", # 3
+                            year=str(datetime.strptime(repo["created_at"], "%Y-%m-%dT%H:%M:%SZ").year) if repo else "", # 4
+                            month_day=str(datetime.strptime(repo["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%b %d")) if repo else "", # 5
+                            topics=[f"{topic} | " for topic in repo["topics"]] # 6
+                        )
+                    )
+            return repos_data
+        except Exception as err:
+            raise Exception(err)
