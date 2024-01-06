4b12b3155ce261ef4a85c297226107ddb0b9d5f4
Kaide Wu <kaidewu@outlook.es>
2024-01-06 18:18:39 +0100

    Update constants

--- portfolio/constants.py
+++ portfolio/constants.py
@@ -1,14 +1,7 @@
-from dotenv import load_dotenv
-from os.path import join, dirname
-import os
-
-dotenv_path = join(dirname(__file__), ".env")
-load_dotenv(dotenv_path)
-
-GITHUB_API_TOKEN = os.environ.get("GITHUB_API_TOKEN")
+USERNAME = "kaidewu"
 PROFILE_URL = "https://media.licdn.com/dms/image/D4D03AQHJJfsn_R49gQ/profile-displayphoto-shrink_800_800/0/1697579469792?e=1704931200&v=beta&t=zBrddj8GNRF03lb9EjOKg8YMqxayk3_VCU7v0NSHSs8"
-GITHUB_URL = "https://github.com/kaidewu"
-LINKEDIN_URL = "https://linkedin.com/in/kaidewu"
+GITHUB_URL = f"https://github.com/{USERNAME}"
+LINKEDIN_URL = f"https://linkedin.com/in/{USERNAME}"
 MAIL = "kaidewu@outlook.es"
-GITHUB_PORTFOLIO_REFLEX_URL = "https://github.com/kaidewu/portfolio-reflex"
-GITHUB_API_URL = "https://api.github.com/users/kaidewu/" # Change the username to your github username
+GITHUB_PORTFOLIO_REFLEX_URL = f"https://github.com/{USERNAME}/portfolio-reflex"
+GITHUB_API_URL = f"https://api.github.com/users/{USERNAME}/repos" # Change the username to your github username
