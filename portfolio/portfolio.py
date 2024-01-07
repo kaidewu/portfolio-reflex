133a2ef230ea27c25a9294f4688bef00699a7d73
Kaide Wu <kaidewu@outlook.es>
2024-01-07 22:59:30 +0100

    adding repositories page

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -3,6 +3,7 @@ import reflex as rx
 from portfolio.pages.index import index
 from portfolio.pages.works import works
 from portfolio.pages.contact import contact
+from portfolio.pages.projects import repository_page
 from portfolio.pages import page404
 
 # Add state and page to the app.
@@ -14,5 +15,6 @@ app = rx.App(
 app.add_page(index)
 app.add_page(works)
 app.add_page(contact)
+app.add_page(repository_page)
 app.add_custom_404_page(page404.index)
 app.compile()
